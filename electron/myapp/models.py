d9c684cc16234c4cad69ba9d841f46d8a2a16e82
shonethomas402 <shonethomassabu2025@mca.ajce.in>
2024-10-14 20:03:18 +0530

    view details of product

--- electron/myapp/models.py
+++ electron/myapp/models.py
@@ -62,14 +62,3 @@ class CartItem(models.Model):
 from django.contrib.auth.models import User
 from django.db import models
 
-class Address(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
-    country = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.street}, {self.city}, {self.state}, {self.zip_code}, {self.country}"
-
